34c9e8c528bde08772464c48b80f0c36f8eb3fb7
Fatih <bozok17@gmail.com>
2023-05-12 18:00:08 +0300

    pre deploy setting

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: ["http://localhost:5173"],
-    origin: "*",
+    origin: "https://pdm-app.onrender.com",
     credentials: true,
   })
 );
